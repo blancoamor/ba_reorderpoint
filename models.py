c3dfdaee5237058377fa35f634a60bb0d722bdeb
morrillo <gustavo.orrillo@moldeo.coop>
2016-08-04 11:17:45 -0300

    Added models

--- models.py
+++ models.py
@@ -14,6 +14,17 @@ _logger = logging.getLogger(__name__)
 
 
 class stock_presupuesto(models.Model):
-	_inherit = 'stock.presupuesto'
+	_name = 'stock.presupuesto'
+	_description = 'Presupuesto periodico para reposicion de stock'
+	
+	warehouse_id = fields.Many2one('stock.warehouse',string='Sucursal')
+	monto_presupuesto = fields.Float('Presupuesto')
+	state = fields.Selection(selection=[('Borrador','En Proceso')],string='Status')
+	presupuesto_lines = fields.One2many(comodel_name='stock.presupuesto.line',inverse_name='presupuesto_id')
 
+class stock_presupuesto_line(models.Model):
+	_name = 'stock.presupuesto.line'
+	_description = 'Lineas de presupuesto para reposicion de stock'
+
+	presupuesto_id = fields.Many2one('stock.presupuesto')
 	
