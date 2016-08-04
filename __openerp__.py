53373d8956edf5a894d579f1a202695e90d723af
morrillo <gustavo.orrillo@moldeo.coop>
2016-08-04 11:08:30 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Blancoamor - Puntos de reposicion',
+    'category': 'POS',
+    'version': '0.1',
+    'depends': ['base','product','procurement','stock'],
+    'data': [
+	#'security/ir.model.access.csv',
+	#'security/security.xml',
+	#'wizard/wizard_view.xml',
+	#'pdv_view.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
---
+++ models.py
@@ -0,0 +1,19 @@
+from openerp import models, fields, api, _
+from openerp.osv import osv
+from openerp.exceptions import except_orm, ValidationError
+from StringIO import StringIO
+import urllib2, httplib, urlparse, gzip, requests, json
+import openerp.addons.decimal_precision as dp
+import logging
+import datetime
+from openerp.fields import Date as newdate
+from datetime import datetime
+
+#Get the logger
+_logger = logging.getLogger(__name__)
+
+
+class stock_presupuesto(models.Model):
+	_inherit = 'stock.presupuesto'
+
+	
