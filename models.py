40d46257a72a389c8e6f37fdc23b7794de8ee30d
morrillo <gustavo.orrillo@moldeo.coop>
2016-08-08 10:44:27 -0300

    More changes

--- models.py
+++ models.py
@@ -67,20 +67,12 @@ class stock_presupuesto(models.Model):
 	@api.one
 	def _compute_presupuesto_previo(self):
 		return_value = None
-		if self.state == 'process':
-			limite = 2
-		else:
-			limite = 1
 		presupuesto_id = self.env['stock.presupuesto'].search([('business_unit','=',self.business_unit.id),\
 					('warehouse_id','=',self.warehouse_id.id),\
-					('state','=','process')],order='date_planned desc',limit=limite)
+					('id','<',self.id),
+					('state','=','process')],order='date_planned desc',limit=1)
 		if presupuesto_id:
-			if limite == 1:
-				self.presupuesto_previo = presupuesto_id.id
-			else:
-				for presupuesto in presupuesto_id:
-					if presupuesto.id != self.id:
-						self.presupuesto_previo = presupuesto.id
+			self.presupuesto_previo = presupuesto_id.id
 		else:
 			self.presupuesto_previo = None
 
